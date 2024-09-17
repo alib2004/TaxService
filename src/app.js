488a5094ae6344aa7807244234335c7634d2ee0c
alib2004 <aliw.b2004@gmail.com>
2024-09-17 16:09:02 +0330

    done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-let hovericon = document.querySelector('.hovericon')  
-let p = document.querySelectorAll('.design p') 
-let iconmenu = document.querySelector('.icon-open-menu') 
-let menumobile = document.querySelector('.menu-mobile') 
-hovericon.addEventListener('mouseover',()=>{
-  p.forEach(element => {
-    element.classList.remove('ops')
+let hovericon = document.querySelector(".hovericon");
+let p = document.querySelectorAll(".design p");
+let iconmenu = document.querySelector(".icon-open-menu");
+let menumobile = document.querySelector(".menu-mobile");
+hovericon.addEventListener("mouseover", () => {
+  p.forEach((element) => {
+    element.classList.remove("ops");
   });
-}) 
-hovericon.addEventListener('mouseout',()=>{
-  p.forEach(element => {
-    element.classList.add('ops')
+});
+hovericon.addEventListener("mouseout", () => {
+  p.forEach((element) => {
+    element.classList.add("ops");
   });
-}) 
-iconmenu.addEventListener('click',()=>{
-  menumobile.classList.toggle('d-none')
-})
+});
+iconmenu.addEventListener("click", () => {
+  menumobile.classList.toggle("d-none");
+});
